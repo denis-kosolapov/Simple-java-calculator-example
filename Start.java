a54c6f48e5c78a91a1340e010de6e711fe12d619
DenisProgrammer <30808396+scisoftdev@users.noreply.github.com>
2020-01-18 10:25:02 +0400

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,28 @@
+package com.LPT.name;
+
+import javax.swing.*;
+
+public class Start {
+
+    private JFrame window;
+
+    public Start() {
+        window = new JFrame("Calculator");
+        window.setSize(300, 500);
+        window.add(new Panel());
+        window.setLocationRelativeTo(null);
+        window.setResizable(false);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+    }
+
+    public static void main(String args[]) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new Start();
+            }
+        });
+    }
+}
+
